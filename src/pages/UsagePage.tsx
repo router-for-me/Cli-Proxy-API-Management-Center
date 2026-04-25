bf030d10fc0cd3972e8969d7764ae4d763b56940
Amamiya23 <zjc020118@gmail.com>
2026-04-25 19:34:21 +0800

    Improve token type distribution readability

--- src/pages/UsagePage.tsx
+++ src/pages/UsagePage.tsx
@@ -2,6 +2,7 @@ import { useState, useMemo, useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   Chart as ChartJS,
+  BarElement,
   CategoryScale,
   LinearScale,
   PointElement,
@@ -46,6 +47,7 @@ import styles from './UsagePage.module.scss';
 
 // Register Chart.js components
 ChartJS.register(
+  BarElement,
   CategoryScale,
   LinearScale,
   PointElement,
